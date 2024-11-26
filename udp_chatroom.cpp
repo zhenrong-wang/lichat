18456fec6278df1ec854a6a7a84808ed25fe89d6
zhenrong-wang <zhenrongwang@live.com>
2024-11-26 21:00:08 +0800

    Added tag and private message

--- udp_chatroom.cpp
+++ udp_chatroom.cpp
@@ -14,9 +14,16 @@
 #include <algorithm>    // For std::find_if
 #include <sstream>      // For stringstream
 #include <unordered_map>
+#include <chrono>
+#include <ctime>
 
+constexpr size_t uid_maxlen = 64;
+constexpr size_t uid_minlen = 4;
+constexpr size_t password_maxlen = 32;
+constexpr size_t password_minlen = 4;
 constexpr uint16_t default_port = 8081;
 constexpr size_t init_buffsize = 1024;
+constexpr char special_chars[] = "~!@#$%^&(){}[]-_=+;:,.<>/|";
 constexpr char main_menu[] = "1. signup\n2. signin\nPlease choose (1 | 2): ";
 constexpr char input_username[] = "Username: ";
 constexpr char input_password[] = "Password: ";
@@ -24,10 +31,18 @@ constexpr char option_error[] = "option error, please input 1 or 2\n";
 constexpr char user_uid_exist[] = "user already exists.\n";
 constexpr char user_uid_error[] = "user does not exist.\n";
 constexpr char password_error[] = "password doesn't match.\n";
+constexpr char length_error[] = "invalid length. user_uid: 4-64. password: 4-32\n";
+constexpr char invalid_char_error[] = "invalid char.\nuser_uid: A(a)lphabet, numbers, and hyphen-\nuser_password: A(a)lphabet, numbers, and special chars, no spaces.\n";
 constexpr char signup_ok[] = "signed up and signed in.\nsend ~:q! to sign out.\n";
 constexpr char signin_ok[] = "signed in.\nsend ~:q! to sign out.\n";
-constexpr char signed_out[] = "[SYSTEM] !!! you have signed out.\n";
+constexpr char signed_out[] = "[SYSTEM] you have signed out.\n";
 constexpr char user_already_signin[] = "user already signed in at: ";
+constexpr char cannot_at_or_to[] = "[SYSTEM] target user not signed in.\n";
+constexpr char been_tagged[] = "[SYSTEM] you've been tagged !\n";
+constexpr size_t MSG_ATTR_LEN = 3;
+constexpr char to_user[MSG_ATTR_LEN] = {'~', '-', '>'};
+constexpr char tag_user[MSG_ATTR_LEN] = {'~', '-', '@'};
+constexpr char user_delim = ':';
 
 // Each user entry include a unique id and a hashed password
 // This approach is not secure enough because we just used ordinary 
@@ -36,6 +51,20 @@ constexpr char user_already_signin[] = "user already signed in at: ";
 struct user_entry {
     std::string user_uid;   // Unique ID
     std::string pass_hash;  // Hashed password
+};
+
+struct msg_attr {
+    uint8_t msg_attr_mask;  // 00 - public & untagged;
+                            // 01 - public but tagged (target_uid and target_ctx_idx valid)
+                            // 02 - private (target_uid and target_ctx_idx valid)
+    std::string target_uid;
+    ssize_t target_ctx_idx;
+
+    msg_attr() {
+        msg_attr_mask = 0;
+        target_uid.clear();
+        target_ctx_idx = -1;
+    }
 };
 
 // Connection Context contains an addr, a bind/empty uid, and a status
@@ -102,6 +131,32 @@ public:
     bool is_in_db(std::string user_uid) {
         return user_db.find(user_uid) != user_db.end();
     }
+
+    // Only Alphabet, numbers, and hyphen are allowed.
+    // Length: 4-64
+    static int user_uid_check(const std::string& str) {
+        if(str.size() < uid_minlen || str.size() > uid_maxlen)
+            return -1;
+        for(auto c : str) {
+            if(!std::isalnum(static_cast<unsigned char>(c)) && c != '-')
+                return 1;
+        }
+        return 0;
+    }
+
+    // Only Alphabet, numbers, and special chars are allowed.
+    // Length: 8-64
+    static int pass_str_check(const std::string& pass_str) {
+        if(pass_str.size() < password_minlen || pass_str.size() > password_maxlen)
+            return -1;
+        std::string special = std::string(special_chars);
+        for(auto c : pass_str) {
+            if(!std::isalnum(static_cast<unsigned char>(c)) && special.find(c) == std::string::npos)
+                return 1;
+        }
+        return 0;
+    }
+
     bool add_user(std::string user_uid, std::string user_password) {
         if(user_uid.empty() || user_password.empty())
             return false;
@@ -200,16 +255,6 @@ public:
         return get_conn_idx(client_addr) != clients.size();
     }
 
-    // Assemble the message header for a connection context
-    std::string assemble_msg_header(conn_ctx& ctx) {
-        struct sockaddr_in addr = *(ctx.get_conn_addr());
-        char ip_cstr[INET_ADDRSTRLEN];
-        std::strncpy(ip_cstr, inet_ntoa(addr.sin_addr), INET_ADDRSTRLEN);
-        std::ostringstream oss;
-        oss << ip_cstr << ":" << ntohs(addr.sin_port) << "\t[UID] " << ctx.get_bind_uid() << ":\t";
-        return oss.str(); 
-    }
-
     // Simplify the socket send function.
     int simple_send(const void *buff, size_t n, struct sockaddr_in client_addr) {
         auto ret = sendto(server_fd, buff, n, MSG_CONFIRM, (struct sockaddr *)&client_addr, sizeof(client_addr));
@@ -230,7 +275,7 @@ public:
     }
 
     // Get the index of clients<> according to a user_uid
-    ssize_t get_client_idx(std::string user_uid) {
+    size_t get_client_idx(std::string user_uid) {
         auto it = std::find_if(clients.begin(), clients.end(), [&user_uid](const conn_ctx& elem) {
             return elem.get_bind_uid() == user_uid;
         });
@@ -240,9 +285,13 @@ public:
             return std::distance(clients.begin(), it);
     }
 
+    bool is_user_signed_in(std::string user_uid) {
+        return get_client_idx(user_uid) != clients.size();
+    }
+
     // Broadcasting to all connected clients (include or exclude current/self).
     size_t system_broadcasting(bool include_self, std::string user_uid, std::string& msg_body) {
-        std::string msg = "[SYSTEM BROADCASTING]: [UID] ";
+        std::string msg = "[SYSTEM BROADCASTING]: [UID]";
         msg += user_uid;
         msg += msg_body;
         size_t sent_out = 0;
@@ -255,6 +304,76 @@ public:
                 ++ sent_out;
         }
         return sent_out;
+    }
+
+    static std::string get_current_time(void) {
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_t = std::chrono::system_clock::to_time_t(now);
+        std::tm* now_tm = std::gmtime(&now_t);
+        std::ostringstream oss;
+        oss << (now_tm->tm_year + 1900) << '-' 
+            << (now_tm->tm_mon + 1) << '-'
+            << (now_tm->tm_mday) << '-'
+            << (now_tm->tm_hour) << ':' << (now_tm->tm_min) << ':' << (now_tm->tm_sec);
+        return oss.str();
+    }
+
+    bool msg_precheck(const std::string& buff_str, struct msg_attr& attr) {
+        auto is_private_msg = (std::memcmp(buff_str.c_str(), to_user, MSG_ATTR_LEN) == 0);
+        auto is_tagged_msg = (std::memcmp(buff_str.c_str(), tag_user, MSG_ATTR_LEN) == 0);
+        if(is_private_msg || is_tagged_msg) {
+            const size_t start_pos = sizeof(to_user);
+            size_t delim_pos = buff_str.find(user_delim, start_pos);
+            std::string target_user;
+            if(delim_pos == std::string::npos) 
+                target_user = buff_str.substr(start_pos); 
+            else
+                target_user = buff_str.substr(start_pos, delim_pos - start_pos);
+            if(all_users.is_in_db(target_user)) { // If the target uid is valid
+                if(!is_user_signed_in(target_user))
+                    return false;   // tagged or private message requires target user signed in.
+                                    // false will bounce the msg back to sender.
+                attr.target_uid = target_user;
+                attr.target_ctx_idx = get_client_idx(target_user);
+                if(!is_private_msg) 
+                    attr.msg_attr_mask = 1; // Public but tagged
+                else
+                    attr.msg_attr_mask = 2; // Private
+                return true; // msg_attr_mask set and return true
+            }
+            // If the target user uid is invalid, do nothing
+            return true;
+        }
+        // If normal message, do nothing.
+        return true;
+    } 
+
+    // Assemble the message header for a connection context
+    std::string assemble_msg_header(const conn_ctx& ctx) {
+        struct sockaddr_in addr = *(ctx.get_conn_addr());
+        char ip_cstr[INET_ADDRSTRLEN];
+        std::strncpy(ip_cstr, inet_ntoa(addr.sin_addr), INET_ADDRSTRLEN);
+        std::string curr_time = get_current_time();
+        std::ostringstream oss;
+        oss << std::endl << curr_time << " [FROM_ADDR] " 
+            << ip_cstr << ":" << ntohs(addr.sin_port) 
+            << " [FROM_UID] " << ctx.get_bind_uid() << ":" << std::endl << "----  ";
+        return oss.str(); 
+    }
+
+    // Must call msg_precheck first!!!
+    void update_msg_buffer(std::vector<char>& buffer, const struct msg_attr& attr, const conn_ctx& ctx) {
+        std::string msg_header = assemble_msg_header(ctx);
+        if(attr.msg_attr_mask != 0)
+            buffer.erase(buffer.begin(), buffer.begin() + MSG_ATTR_LEN + attr.target_uid.size() + 1);
+        if(attr.msg_attr_mask == 1) 
+            msg_header += (std::string("@@") + attr.target_uid + std::string(" "));
+        else if(attr.msg_attr_mask == 2)
+            msg_header += (std::string("**") + std::string(" "));
+        buffer.insert(buffer.begin(), msg_header.c_str(), msg_header.c_str() + msg_header.size());
+        buffer.back() = '\n';
+        buffer.push_back('\n');
+        buffer.push_back('\0');
     }
 
     // Main processing method.
@@ -307,6 +426,15 @@ public:
                     }
                 }
                 else if(stat == 2 || stat == 3) {
+                    int flag = all_users.user_uid_check(buff_str);
+                    if(flag == -1) {
+                        simple_send(length_error, sizeof(length_error), client_addr);
+                        client.reset_conn(); continue;
+                    }
+                    if(flag == 1) {
+                        simple_send(invalid_char_error, sizeof(invalid_char_error), client_addr);
+                        client.reset_conn(); continue;
+                    }
                     if(stat == 2) {
                         if(all_users.is_in_db(buff_str)) {
                             simple_send(user_uid_exist, sizeof(user_uid_exist), client_addr);
@@ -340,18 +468,27 @@ public:
                     }
                 }
                 else if(stat == 4 || stat == 5) {
+                    int flag = all_users.pass_str_check(buff_str);
+                    if(flag == -1) {
+                        simple_send(length_error, sizeof(length_error), client_addr);
+                        client.reset_conn(); continue;
+                    }
+                    if(flag == 1) {
+                        simple_send(invalid_char_error, sizeof(invalid_char_error), client_addr);
+                        client.reset_conn(); continue;
+                    }
                     std::string user_uid = client.get_bind_uid();
                     if(stat == 4) {
                         all_users.add_user(user_uid, buff_str);
                         simple_send(signup_ok, sizeof(signup_ok), client_addr);
-                        std::string msg_body = " signed up and in !!!\n";
+                        std::string msg_body = " signed up and in !\n";
                         system_broadcasting(false, user_uid, msg_body);
                         client.set_status(6);
                     }
                     else {
                         if(all_users.is_user_pass_valid(user_uid, buff_str)) {
                             simple_send(signin_ok, sizeof(signin_ok), client_addr);
-                            std::string msg_body = " signed in !!!\n";
+                            std::string msg_body = " signed in !\n";
                             system_broadcasting(false, user_uid, msg_body);
                             client.set_status(6);
                         }
@@ -365,17 +502,34 @@ public:
                     std::string user_uid = client.get_bind_uid();
                     if(buff_str == "~:q!") {
                         simple_send(signed_out, sizeof(signed_out), client_addr);
-                        std::string msg_body = " signed out !!!\n";
+                        std::string msg_body = " signed out !\n";
                         system_broadcasting(false, user_uid, msg_body);
                         client.reset_conn();
                     }
                     else {
-                        buffer[bytes_recv - 1] = '\n';
-                        msg_header = assemble_msg_header(client);
-                        buffer.insert(buffer.begin(), msg_header.c_str(), msg_header.c_str() + msg_header.size());
-                        for(auto& item : clients) {
-                            if(item.get_status() == 6)
-                                simple_send(buffer.data(), buffer.size(), *(item.get_conn_addr()));
+                        struct msg_attr attr;
+                        if(!msg_precheck(buff_str, attr)) {
+                            simple_send(cannot_at_or_to, sizeof(cannot_at_or_to), client_addr);
+                            continue;
+                        }
+                        update_msg_buffer(buffer, attr, client);
+                        if(attr.msg_attr_mask == 0) {
+                            for(auto& item : clients) {
+                                if(item.get_status() == 6)
+                                    simple_send(buffer.data(), buffer.size(), *(item.get_conn_addr()));
+                            }
+                        }
+                        else if(attr.msg_attr_mask == 1) {
+                            for(auto& item : clients) {
+                                if(item.get_status() == 6) {
+                                    if(item.get_bind_uid() == attr.target_uid)
+                                        simple_send(been_tagged, sizeof(been_tagged), *(clients[attr.target_ctx_idx].get_conn_addr()));
+                                    simple_send(buffer.data(), buffer.size(), *(item.get_conn_addr()));
+                                }
+                            }
+                        }
+                        else {
+                            simple_send(buffer.data(), buffer.size(), *(clients[attr.target_ctx_idx].get_conn_addr()));
                         }
                     }
                 }
