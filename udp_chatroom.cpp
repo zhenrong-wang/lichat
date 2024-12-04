a6d9720355e5d2483391d2a556c6fc0a50b6c8bb
zhenrong-wang <zhenrongwang@live.com>
2024-12-04 18:19:56 +0800

    WIP

--- udp_chatroom.cpp
+++ udp_chatroom.cpp
@@ -653,7 +653,7 @@ public:
         return buffer[0] == 0x10;
     }
     bool is_error_msg() const {
-        return (buffer[0] == 0xFF) | buffer[0] == 0xEF
+        //return (buffer[0] == 0xFF) | buffer[0] == 0xEF
     }
 };
 
